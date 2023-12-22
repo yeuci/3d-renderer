09e33d8c179f18e448579c4642a809b358e79bb0
yeuci <nebbbnn@gmail.com>
2023-12-21 20:13:45 -0500

    clear event buffer

--- src/main.c
+++ src/main.c
@@ -58,17 +58,17 @@ void setup(void) {
 
 void process_input(void) {
   SDL_Event event;
-  SDL_PollEvent(&event);
-
-  switch (event.type) {
-    case SDL_QUIT:
-      is_running = false;
-      break;
-    case SDL_KEYDOWN:
-      if (event.key.keysym.sym == SDLK_ESCAPE) {
-        is_running = false;
-      }
-      break;
+  while(SDL_PollEvent(&event)) {
+    switch (event.type) {
+     case SDL_QUIT:
+       is_running = false;
+       break;
+     case SDL_KEYDOWN:
+       if (event.key.keysym.sym == SDLK_ESCAPE) {
+         is_running = false;
+       }
+       break;
+    }
   }
 }
 
